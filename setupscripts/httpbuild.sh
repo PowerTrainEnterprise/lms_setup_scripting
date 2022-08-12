8fa4c3edc3770f7269d723f5369498b483a8db6f
Powertrain - User Account <software@powertrain.com>
2022-08-12 11:19:49 -0400

    Update httpbuild.sh

--- setupscripts/httpbuild.sh
+++ setupscripts/httpbuild.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
+ 
+cp -f /etc/httpd/httpd-http.conf /etc/httpd/conf/apache2.conf
 
-if [ "$HTTPS_TOGGLE" == "1" ]
-then
-	cp -f /etc/httpd/httpd-ssl.conf /etc/httpd/conf/httpd.conf
-	aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_CERT_NAME /etc/pki/tls/certs/cert.pem
-    aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_PRIVATE_KEY /etc/pki/tls/certs/privatekey.pem
-	
-	if [ "$SSL_CHAIN_TOGGLE" == "1" ]
-	then
-		aws s3 cp s3://$S3_BUCKET_NAME/$S3_ENVIRONMENT/moodle/$S3_CHAIN_CERT /etc/pki/tls/certs/chain.pem
-		chown apache:apache /etc/pki/tls/certs/chain.pem
-		sed -i -e "s/#SSLCertificateChainFile/SSLCertificateChainFile/g" /etc/httpd/conf/httpd.conf
-	fi
-	
-	chown apache:apache /etc/pki/tls/certs/cert.pem
-	chown apache:apache /etc/pki/tls/certs/privatekey.pem
-else 
-	cp -f /etc/httpd/httpd-http.conf /etc/httpd/conf/httpd.conf
-fi
+sed -i -e "s/{WEB_HOSTNAME}/$RAW_HOSTNAME/g" /etc/httpd/conf/apache2.conf
 
-sed -i -e "s/{WEB_HOSTNAME}/$RAW_HOSTNAME/g" /etc/httpd/conf/httpd.conf
-
-echo "HTTPD Built"
+echo "apache2 Built"
