9b30c2dba3a8d985f623995635ce53e2295e0b20
Powertrain - User Account <software@powertrain.com>
2022-09-16 11:09:16 -0400

    Update moodle_container_post_create.sh

--- setupscripts/moodle_container_post_create.sh
+++ setupscripts/moodle_container_post_create.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 echo "Moodle Container Instance Launched. Starting Post Create commands."
 
+echo "Copying Custom Config File."
+cp /moodle_docker_config.php /workspaces/moodle_container_instance/config.php
+echo "Custom Config File Copied!"
+
 echo "Starting Service: APACHE2"
 systemctl enable apache2
-service apache2 start 
+#service apache2 start 
 echo "APACHE2 Started"
 
 echo "Starting Service: postgresql"
 systemctl enable postgresql
-service postgresql start
+#service postgresql start
 echo "postgresql Started"
