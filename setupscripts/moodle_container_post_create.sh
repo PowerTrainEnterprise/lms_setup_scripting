7f876380b801c159fcd2c001867de58a4575f6ab
Powertrain - User Account <software@powertrain.com>
2022-09-22 11:01:58 -0400

    Update moodle_container_post_create.sh

--- setupscripts/moodle_container_post_create.sh
+++ setupscripts/moodle_container_post_create.sh
@@ -6,16 +6,18 @@ cp /moodle_docker_config.php /workspaces/moodle_container_instance/config.php
 echo "Custom Config File Copied!"
 
 echo "Starting Service: APACHE2"
-#systemctl enable apache2
+systemctl enable apache2
 service apache2 start 
 echo "APACHE2 Started"
 
 echo "Starting Service: postgresql"
-#systemctl enable postgresql
+systemctl enable postgresql
 service postgresql start
 echo "postgresql Started"
 
-psql "CREATE USER root WITH PASSWORD 'root';"
-psql "CREATE USER moodle WITH PASSWORD 'moodle';"
-psql "CREATE DATABASE moodle WITH OWNER moodle;"
-psql moodle < /moodle_preconfigured_database.sql
+chmod -R 777 /moodledata
+
+psql postgres -c "CREATE USER root WITH PASSWORD 'root';"
+psql postgres -c "CREATE USER moodle WITH PASSWORD 'moodle';"
+psql postgres -c "CREATE DATABASE moodle WITH OWNER moodle;"
+#psql moodle < /moodle_preconfigured_database.sql
