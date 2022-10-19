3fef82fed872ee3a750588d6c57cd44744ac612b
Powertrain - User Account <software@powertrain.com>
2022-10-19 13:55:30 -0400

    Update moodle_container_post_create.sh

--- setupscripts/moodle_container_post_create.sh
+++ setupscripts/moodle_container_post_create.sh
@@ -27,9 +27,14 @@ echo "postgresql Started"
 
 chmod -R 777 /moodledata
 
+echo "Copying submodules."
+git submodule update --init
+echo "Copied submodules."
+
 su -c "psql -c \"CREATE USER root WITH PASSWORD 'root'\"" postgres
 su -c "psql -c \"CREATE USER moodle WITH PASSWORD 'moodle';\"" postgres
 su -c "psql -c \"CREATE DATABASE moodle WITH OWNER moodle;\"" postgres
+su -c "psql \i /moodle_preconfigured_database.sql\"" postgres
 #su -c "psql -c \"moodle < /moodle_preconfigured_database.sql\"" postgres
 #su postgres psql -c "CREATE USER root WITH PASSWORD 'root';"
 #psql postgres -c "CREATE USER moodle WITH PASSWORD 'moodle';"
