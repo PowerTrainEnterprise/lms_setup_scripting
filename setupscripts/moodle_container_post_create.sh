c915b9d7ca7d1b336852c99d88366e008708810b
Powertrain - User Account <software@powertrain.com>
2022-10-25 10:51:27 -0400

    Update moodle_container_post_create.sh

--- setupscripts/moodle_container_post_create.sh
+++ setupscripts/moodle_container_post_create.sh
@@ -36,7 +36,7 @@ su -c "psql -c \"ALTER USER root WITH SUPERUSER\"" postgres
 su -c "psql -c \"CREATE USER moodle WITH PASSWORD 'moodle';\"" postgres
 su -c "psql -c \"ALTER USER moodle WITH SUPERUSER\"" postgres
 su -c "psql -c \"CREATE DATABASE moodle WITH OWNER moodle;\"" postgres
-su -c "pg_restore -f \"/moodle_preconfigured_database.sql\"" postgres
+@su -c "pg_restore -f \"/moodle_preconfigured_database.sql\"" postgres
 #su -c "psql -c \"moodle < /moodle_preconfigured_database.sql\"" postgres
 #su postgres psql -c "CREATE USER root WITH PASSWORD 'root';"
 #psql postgres -c "CREATE USER moodle WITH PASSWORD 'moodle';"
