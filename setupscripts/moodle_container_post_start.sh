768aa78e52942a716b4830c10502c3b614d717ba
Powertrain - User Account <software@powertrain.com>
2023-01-11 11:57:10 -0500

    Update moodle_container_post_start.sh

--- setupscripts/moodle_container_post_start.sh
+++ setupscripts/moodle_container_post_start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-echo "Starting Post Start commands."
-echo "Starting Service: APACHE2"
 systemctl enable apache2
 service apache2 start 
-echo "APACHE2 Started"
-
-echo "Starting Service: postgresql"
 systemctl enable postgresql
 service postgresql start
-echo "postgresql Started"
+echo "Services Restarted: apache2, postgresql"
